9c38819ec587db94e1d7868eac38665db9b1b54a
ukasru <ukas.ru@gmail.com>
2021-02-21 14:13:33 +0300

    Initial commit

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "crypto-monitor-db.firebaseapp.com",
+  databaseURL: "https://crypto-monitor-db-default-rtdb.firebaseio.com",
+  projectId: "crypto-monitor-db",
+  storageBucket: "crypto-monitor-db.appspot.com",
+  messagingSenderId: "832771322228",
+  appId: "1:832771322228:web:c105206b40117a02e27c21",
+  measurementId: "G-49Y0SXLBJC",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth;
+export const db = firebase.database();
